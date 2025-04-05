3de763bf115ff21aa0652e0a4125cf89e2d8e668
Marzia Jalili <astronomyfancy@gmail.com>
2025-04-05 14:32:13 +0430

    added to top button!

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,18 @@ const App = () => {
   return (
     <>
       {/* the scroll progress bar */}
-
       <div className="progress-bar rounded-full z-100 fixed top-0 right-0 w-[6px] bg-linear-to-t from-Lime-Green to-Bright-Cyan"></div>
       <div className="scroll-path rounded-full z-100 fixed top-0 right-0 w-[6px] h-[10px] bg-Bright-Cyan"></div>
-
       {/* main content of the page */}
-
       <Header darkMode={darkMode} />
+
+      {/* to top button */}
+      <a
+        href="#"
+        className="fixed z-100 py-2 top-[50%] cursor-pointer px-1 pr-3 hover:px-4 font-extrabold text-white bg-gradient-to-r  from-Lime-Green to-Bright-Cyan bg-Bright-Red rounded-tr-full rounded-br-full duration-300 text-bold hover:brightness-120 hover:opacity-100 hover:shadow-[0_0_30px_hsla(136,65%,51%,1)]"
+      >
+        ↑
+      </a>
       <main>
         <Cta darkMode={darkMode} setDarkMode={setDarkMode} />
         <Features />
